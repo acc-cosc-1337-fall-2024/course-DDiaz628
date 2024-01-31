808d67773673685b811c58608695806e076a03b2
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-01-31 00:16:47 +0000

    Using auto keyword

--- src/examples/01_data_types/03_auto/main.cpp
+++ src/examples/01_data_types/03_auto/main.cpp
@@ -1,5 +1,17 @@
+#include <iostream>
+
+using std::cout;
+
 int main()
 {
+    auto num = 10;
+    cout<<"integer:  "<<num<<"\n";
+
+    auto num1 = 10.5;
+    cout<<"double:  "<<num1<<"\n";
+
+    auto char1 = 'a';
+    cout<<"char:  "<<char1<<"\n";
 
     return 0;
 }
