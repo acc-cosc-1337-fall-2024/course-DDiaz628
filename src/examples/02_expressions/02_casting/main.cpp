c4125af894e6d13d836dd2eacf131a838a727df9
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-01 23:56:28 +0000

    Convert to double

--- src/examples/02_expressions/02_casting/main.cpp
+++ src/examples/02_expressions/02_casting/main.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+#include "casting.h"
+
+using std::cin; using std::cout;
 
 int main()
 {
+    auto years = 0;
+    auto rate = 1.0;
+
+    cout<<"enter years: ";
+    cin>>years;
+    cout<<"enter rate: ";
+    cin>>rate;
+
+    auto result = convert_to_double(years,rate);
+
+    cout<<"value is: "<<result<<"\n";
 
     return 0;
 }
