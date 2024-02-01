cf815d23d9b4125be866eedfbd145518e9ee6c6a
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-01 23:30:14 +0000

    Using String

--- src/examples/01_data_types/05_string/main.cpp
+++ src/examples/01_data_types/05_string/main.cpp
@@ -1,7 +1,15 @@
+#include <string>
+#include <iostream>
+
+using std::string;
+using std::cout;
 
 //create string variable, initialize and display
 int main()
 {
+    string lang = "C++";
+
+    cout<<lang<<"\n";
 
     return 0;
 }
