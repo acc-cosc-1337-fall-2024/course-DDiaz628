6ae542632caf390bcdff74be8101b6b698473426
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-02 00:31:08 +0000

    Overflow

--- src/examples/02_expressions/03_overflow/main.cpp
+++ src/examples/02_expressions/03_overflow/main.cpp
@@ -1,6 +1,18 @@
+#include <iostream>
+#include <climits>
+
+using std::cout;
+
 
 int main()
 {
+    //overflow example
+    auto max_int = INT_MAX;
+    cout<<"Max int: "<<max_int<<"\n";
+
+    max_int = max_int + 1;
+    cout<<"Max int: "<<max_int<<"\n";
+
 
     return 0;
 }
