3577d6c62c4b22676f170afc968343a1789b7275
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-06 22:55:45 +0000

    Constants

--- src/examples/02_expressions/04_constants/constants.h
+++ src/examples/02_expressions/04_constants/constants.h
@@ -1,4 +1,7 @@
+const double Pi = 3.14159;//read only var - global to this header file
+
 /*
 Create double value-return function get_area_of_circle with 
 double paramter (radius)
 */
+double get_area_of_circle(double radius);
--- test/examples/02_expressions/02_expressions_tests.cpp
+++ test/examples/02_expressions/02_expressions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "casting.h"
+#include "constants.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -16,4 +17,8 @@ TEST_CASE("Test implicit data type casting double to int","Verify that double to
 
 TEST_CASE("Test explicit type casting") {
 	REQUIRE(static_cast_double_int(10.99) == 10);
+}
+
+TEST_CASE("Test constant value-get area of circle") {
+	REQUIRE(get_area_of_circle(10) == 314.159);
 }
