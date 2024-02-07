fa165945d7c19918a5f9f1112d5ce25f1c278bb3
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-07 00:15:33 +0000

    AND, OR, NOT

--- test/examples/03_conditionals/03_conditionals_tests.cpp
+++ test/examples/03_conditionals/03_conditionals_tests.cpp
@@ -1,6 +1,26 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "if.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("Verify AND truth table") {
+	REQUIRE(get_and_result(true, true) == true);
+	REQUIRE(get_and_result(true, false) == false);
+	REQUIRE(get_and_result(false, true) == false);
+	REQUIRE(get_and_result(false, false) == false);
+}
+
+TEST_CASE("Verify OR truth table") {
+	REQUIRE(get_or_result(true, true) == true);
+	REQUIRE(get_or_result(true, false) == true);
+	REQUIRE(get_or_result(false, true) == true);
+	REQUIRE(get_or_result(false, false) == false);
+}
+
+TEST_CASE("Verify NOT operator") {
+	REQUIRE(get_not_result(true) == false);
+	REQUIRE(get_not_result(false) == true);
 }
