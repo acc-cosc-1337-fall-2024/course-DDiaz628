f99ef91f1c5968338e238679d75ffd14a7de5dbb
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-21 00:06:08 +0000

    function value parameter

--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,14 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "val_ref.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
+}
+
+TEST_CASE("val_ref") {//copy parameter
+	int num = 5; //function local variable
+	value_params(num);
+	
+	REQUIRE(num == 5);
 }
