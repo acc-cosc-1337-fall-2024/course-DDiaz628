e8767b6806888ebdfd58ef9e4a4666d7766cf8f4
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-22 21:27:37 +0000

    const_reference_params

--- src/examples/05_functions/03_val_ref_params/val_ref.cpp
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -10,3 +10,9 @@ void reference_params(int& num)
     num = 10;
 
 }
+
+void const_reference_params(const int& num)
+{
+    //num = 90;  statement shows that we can protect variable data by using const which means read-only
+
+}
