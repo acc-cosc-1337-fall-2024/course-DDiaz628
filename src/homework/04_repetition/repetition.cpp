c4cbab3f37588a36ac53ef5ccb9a60bc7a946265
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-22 23:31:23 +0000

    Workinng on hw1 part1 (02-22-24)

--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,38 @@
+#include "repetition.h"
 //add include statements
 
 //add function(s) code here
+int factorial(int num)
+{
+    int sum = 1;
+
+    while (num > 0)
+    {
+        sum *= num;
+        num--;
+    }
+    
+    return sum;
+}
+
+int gcd(int num1, int num2)
+{
+    //int result = 0;
+
+    while(num1 != num2)
+    {
+        if (num1 < num2)
+        {
+            int i = num2;
+            num2 = num1;
+            num1 = i;
+        }
+        
+        else if (num1 > num2)
+        {
+            num1 -= num2;
+        }
+    }
+
+    return num1;
+}
--- src/homework/04_repetition/repetition.h
+++ src/homework/04_repetition/repetition.h
@@ -1,5 +1,11 @@
+#include <iostream>
+
+using std::cout; using std::cin;
+
 //add include statements
 
 /*
 Write prototype for function(s)
 */
+int factorial(int num);
+int gcd(int num1, int num2);
