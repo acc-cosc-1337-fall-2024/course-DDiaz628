0b57aabc99c87a0f234d1d51d88dbe4eb98e782a
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-23 00:42:23 +0000

    Function overload

--- src/examples/05_functions/04_static/static.h
+++ src/examples/05_functions/04_static/static.h
@@ -3,3 +3,6 @@
 void use_none_static_variable();
 void use_static_variable();
 void use_static_variable1();
+
+double get_gross_pay(int hours, double rate);
+double get_gross_pay(int yearly_salary);
--- test/examples/05_functions/05_functions_tests.cpp
+++ test/examples/05_functions/05_functions_tests.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "val_ref.h"
+#include "static.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
@@ -17,4 +18,12 @@ TEST_CASE("test reference_params") {
 	int num = 5; //local reverence variable
 	reference_params(num);//pass the address where num is stored
 	REQUIRE(num == 10);
+}
+
+TEST_CASE("test get_gross_pay w 2 param") {
+	REQUIRE(get_gross_pay(40, 20) == 800);
+}
+
+TEST_CASE("test get_gross_pay w 1 param") {
+	REQUIRE(get_gross_pay(12000) == 1000);
 }
