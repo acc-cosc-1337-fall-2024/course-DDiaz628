c654332ee46b1cace7af8e2bd56b8e0e1be0692b
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-23 00:08:34 +0000

    variable scope

--- src/examples/05_functions/03_val_ref_params/val_ref.cpp
+++ src/examples/05_functions/03_val_ref_params/val_ref.cpp
@@ -8,11 +8,28 @@ void value_params(int num)
 void reference_params(int& num)
 {
     num = 10;
-
+    int rate = federal_tax_rate;
 }
+
+//int rate = 10; - different variable(gloabal)
 
 void const_reference_params(const int& num)
 {
     //num = 90;  statement shows that we can protect variable data by using const which means read-only
 
+}
+
+void access_global_variable()
+{
+   // federal_tax_rate = .5; can't mod if const
+    int rate = 1;
+   if(true)//same holds for switch, whiile, for
+   {
+        //federal_tax_rate;
+        //int rate = 1;
+
+        rate = 10;
+   }
+
+   //rate = 5;
 }
--- src/examples/05_functions/03_val_ref_params/val_ref.h
+++ src/examples/05_functions/03_val_ref_params/val_ref.h
@@ -1,3 +1,7 @@
+const double federal_tax_rate = .1;//global variable - read/write
+
 void value_params(int num);//value
 void reference_params(int& num);//reference
 void const_reference_params(const int& num);//protect data with const - read only
+
+void access_global_variable();
