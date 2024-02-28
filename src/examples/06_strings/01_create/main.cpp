88e2cd8f02aaed74fabd86bb7ddeed23b7840ce0
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-02-28 00:31:20 +0000

    String size & capacity

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
 
-using std::cout;
+using std::cout; using std::string;
 
 int main()
 {
-        char letter = 'a';
+        //char letter = 'a';
+        string lang = "C++";
 
-        cout<<letter<<"\n";
+        cout<<lang<<"\n";
+        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+
+        lang.append("123456789012");
+        
+        cout<<lang<<"\n";
+        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+
+        lang.push_back('3');
+
+        cout<<lang<<"\n";
+        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+
     return 0;
 }
