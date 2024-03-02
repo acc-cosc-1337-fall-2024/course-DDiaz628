e1ba1a97d6d3a7967673599719c17b40781835fa
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-03-02 17:59:32 +0000

    Douglas Diaz - HW5

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,94 @@
 //add include statements
+#include "func.h"
+
 
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double sum = 0;
+    double result;
+
+    
+    
+    for(int i = 0; i < dna.size(); i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C')
+        {
+            sum += 1;
+        }
+
+        else if(dna[i] == 'A' || dna[i] == 'T')
+        {
+            sum += 0;
+        }
+
+        else
+        {
+            return -1;
+        }
+
+    }
+
+    result = sum / dna.size();
+
+    return result;
+
+}
+
+string get_dna_complement(string dna)
+{
+    string rev_dna = reverse_string(dna);
+
+    int num = 0;
+
+    while(num < rev_dna.size())
+    {
+        if (rev_dna[num] == 'A')
+        {
+            rev_dna[num] = 'T';
+        }
+
+        else if (rev_dna[num] == 'T')
+        {
+            rev_dna[num] = 'A';
+        }
+
+        else if (rev_dna[num] == 'C')
+        {
+            rev_dna[num] = 'G';
+        }
+
+        else if (rev_dna[num] == 'G')
+        {
+            rev_dna[num] = 'C';
+        }
+
+        else //if(rev_dna[num] != 'A' && rev_dna[num] != 'T' && rev_dna[num] != 'C' && rev_dna[num != 'G'])
+        {
+            rev_dna = "";
+            //num = -2;
+            //cout<<"\n Invalid DNA string.\n\n Returning to Main Menu\n\n";
+            return "0";
+
+        }
+
+        num++;
+    }
+
+    return rev_dna;
+}
+
+string reverse_string(string dna)
+{
+    int num = dna.size() - 1;
+    string rev_dna = "";
+
+    do
+    {
+        rev_dna += dna[num];
+        num--;
+    } while (num >= 0);
+    
+
+    return rev_dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,89 @@
+#include "func.h"
 
 int main() 
 {
+	int option = 0;
+	int exit = 0;
+	string dna = "";
+	double result_gc = 0;
+	string result_dna_complement = "";
+	
+	do
+	{
+		cout<<"\tMain Menu\n";
+		cout<<"1-Get GC Content\n";
+		cout<<"2-Get DNA Complement\n";
+		cout<<"3-Exit\n";
+		cin>>option;
+
+		if(option == 1)
+		{
+			cout<<"\nEnter DNA string: \n";
+			cin>>dna;
+			result_gc = get_gc_content(dna);
+			if(result_gc == -1)
+			{
+				cout<<"\nInvalid DNA string.\n\nReturning to Main Menu\n\n";
+			}
+
+			else
+			{
+				cout<<"\nGC content of "<<dna<<" is: "<<result_gc<<"\n\nReturning to Main Menu\n\n";
+			}
+		}
+
+		else if(option == 2)
+		{
+			cout<<"\nEnter DNA string: \n";
+			cin>>dna;
+			result_dna_complement = get_dna_complement(dna);
+
+			if(result_dna_complement == "0")
+			{
+				cout<<"\nInvalid DNA string.\n\nReturning to Main Menu\n\n";
+			}
+
+			else
+			{
+				cout<<"\nDNA complement for "<<dna<<" is "<<result_dna_complement<<"\n\nReturning to Main Menu\n\n";
+			}
+		}
+
+		else if(option !=1 && option !=2 && option != 3 && option != 0)
+		{
+			cout<<"\nInvalid option\n\n";
+		}
+
+		while(option == 3)
+		{
+			cout<<"\nAre you sure you want to exit?\n";
+			cout<<"1-Yes\n2-No\n";
+			cin>>exit;
+			
+			if (exit == 1)
+			{
+				cout<<"\nGoodbye\n";
+				break;
+			}
+
+			else if(exit == 2)
+			{
+				option = 0;
+				cout<<"\nReturn to Main Menu\n\n";
+				exit = 0;
+
+			}
+
+			else
+			{
+				cout<<"\nInvalid option\n";
+			}
+
+		}
+		
+
+	} while (option != 3);
+	
 
 	return 0;
 }
