c6f7a0ab92239644ed8bd0f33f144c6a03d9d738
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-03-06 00:00:04 +0000

    Vector size and capacity

--- src/examples/07_vector/02_functions/main.cpp
+++ src/examples/07_vector/02_functions/main.cpp
@@ -1,4 +1,19 @@
+#include <vector>
+#include <iostream>
+
+using std::vector; using std:: cout;
+
 int main()
 {
+    vector<int> nums{7, 2, 9};
+
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n";
+
+    nums.push_back(5);
+
+    cout<<"size: "<<nums.size()<<"\n";
+    cout<<"capacity: "<<nums.capacity()<<"\n";
+
     return 0;
 }
