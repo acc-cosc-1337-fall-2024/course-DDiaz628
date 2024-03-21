438e826671ac5c5f6f76efd62f8fe6b877387de5
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-03-21 22:56:28 +0000

    Class Basics

--- src/examples/06_strings/01_create/main.cpp
+++ src/examples/06_strings/01_create/main.cpp
@@ -5,24 +5,24 @@ using std::cout; using std::string;
 
 int main()
 {
-        //char letter = 'a';
-        string lang = "C++";
+    //char letter = 'a';
+    string lang = "C++";
 
-        cout<<lang<<"\n";
-        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
-        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
 
-        lang.append("123456789012");
-        
-        cout<<lang<<"\n";
-        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
-        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+    lang.append("123456789012");
+    
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
 
-        lang.push_back('3');
+    lang.push_back('3');
 
-        cout<<lang<<"\n";
-        cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
-        cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
+    cout<<lang<<"\n";
+    cout<<"Size of string: "<<lang.size()<<"\n"; //number of characters in the string
+    cout<<"Capacity of string: "<<lang.capacity()<<"\n"; //Max number of strings the lang variable can hold
 
 
 
