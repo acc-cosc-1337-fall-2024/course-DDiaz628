ac0256237ed7183e8513fbb00074122f1d94c44a
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-03-28 22:56:41 +0000

    BankAccount private function generate random balance

--- src/examples/08_classes/01_bank/bank_account.h
+++ src/examples/08_classes/01_bank/bank_account.h
@@ -1,5 +1,6 @@
 //bank_account.h
 #include <iostream>
+#include <stdlib.h>//Access to rand() function
 
 #ifndef BANK_ACCOUNT_H
 #define BANK_ACCOUNT_H
@@ -8,7 +9,7 @@ class BankAccount
 {
 
 public: //other regions of code, test, other function, other class can see
-    BankAccount(){}//Default Constructor
+    BankAccount(){get_balance_from_db();}//Default Constructor
     BankAccount(int b) : balance(b){/*empty code block*/}
     
     int get_balance(){return balance;}
@@ -17,10 +18,12 @@ public: //other regions of code, test, other function, other class can see
 
 private://only BankAccount can read/write to balance directly
     int balance{0}; //Initialize balance to 0 on class creation    
+    void get_balance_from_db();
 };
 
 #endif
 
-void display_menu();
+/*void display_menu();
 void run_menu(BankAccount& account);
 void handle_menu(int choice, BankAccount& account);
+*/
--- src/examples/08_classes/01_bank/main.cpp
+++ src/examples/08_classes/01_bank/main.cpp
@@ -1,22 +1,29 @@
 #include "atm.h"
 #include "bank_account.h"
 #include <iostream>
+#include <time.h>
 
 using std::cout;
 
 
 int main()
 {
+	srand(time(NULL));
 	//assume other class retrieved balance from database
 
 
-	BankAccount account(500); //create variable/object
+	BankAccount account; //create variable/object
+	cout<<account.get_balance()<<"\n";
 
-	ATM atm(account);
+	BankAccount account2; //create variable/object
+	cout<<account2.get_balance()<<"\n";
+
+	/*ATM atm(account);
 
 	run_menu(atm);
 
 	cout<<"Balance: "<< account.get_balance()<<"\n";
+	*/
 
 	return 0;
 }
