aec2497a409f804a47d15161a00b27d9360ade5c
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-03-30 13:29:36 +0000

    Douglas Diaz - HW6

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,6 +1,35 @@
+#include "tic_tac_toe.h"
 
 int main() 
 {
+	TicTacToe game;
+	string p1;
+	char option;
+
+	do
+	{
+		cout<<"Enter player 1: ";
+		cin>>p1;
+
+		game.start_game(p1);
+
+		int position;
+
+		while(!game.game_over())
+		{
+			cout<<"\nEnter a position: ";
+			cin>>position;
+			game.mark_board(position);
+			game.display_board();
+
+
+		}
+
+		cout<<"\nPlay again?\nEnter y or Y: ";
+		cin>>option;
+	
+	} while (option == 'y' || option == 'Y');
+	
 
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using std::string; using std::cout; using std::cin;
 //h
+#ifndef TIC_TAC_TOE_H
+#define TIC_TAC_TOE_H
+
+class TicTacToe
+{
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const {return player;}
+    void display_board() const;
+    
+
+private:
+    string player;
+    std::vector<string> pegs{9, " "};
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+};
+
+
+#endif
