71a48410fe6cd6c883871a4830ca8c8f2970ed6d
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-04-02 23:52:26 +0000

    Pointer can show different points

--- src/examples/11_memory/01_ref_pointers/main.cpp
+++ src/examples/11_memory/01_ref_pointers/main.cpp
@@ -5,7 +5,18 @@ using std::cout;
 
 int main() 
 {
-	int num1 = 5;
+	int num1 = 5, num2 = 10;
+	int* num_ptr; //don't have to point at anything
+	cout<<"Address of num1: "<<&num1<<"\n";
+
+	num_ptr = &num1;
+	cout<<"Address othat num_pte points to: "<<num_ptr<<"\n";
+	cout<<"Value of the variable mum_ptr points to: "<<*num_ptr<<"\n";
+
+	num_ptr = &num2;
+	cout<<*num_ptr<<"\n";
+
+	/*int num1 = 5;
 	int& num1_ref = num1;
 
 	cout<<num1_ref<<"\n";
@@ -21,6 +32,7 @@ int main()
 	*num2_ptr = 10;
 	cout<<*num2_ptr<<"\n";
 	cout<<num2<<"\n";
+	*/
 	
 	return 0;
 }
