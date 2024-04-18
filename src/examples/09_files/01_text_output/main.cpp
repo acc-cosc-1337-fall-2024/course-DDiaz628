8b3b4c1a5cd0794bef23b10c665e10fb0c9732a2
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-04-18 23:49:04 +0000

    File i/o temp

--- src/examples/09_files/01_text_output/main.cpp
+++ src/examples/09_files/01_text_output/main.cpp
@@ -1,9 +1,10 @@
 #include "text_output.h"
+#include "temperature_data.h"
 
 int main()
 {
     //output_data_to_file();
-    input_data_from_file();
+    //input_data_from_file();
 
     return 0;
 }
