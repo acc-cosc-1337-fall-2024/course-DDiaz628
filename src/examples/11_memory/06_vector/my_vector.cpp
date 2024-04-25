d6d9fe73195908a24ee30e3e52f7924c67ef8864
DDiaz628 <157077996+DDiaz628@users.noreply.github.com>
2024-04-25 23:43:17 +0000

    Constructor/Destructor

--- src/examples/11_memory/06_vector/my_vector.cpp
+++ src/examples/11_memory/06_vector/my_vector.cpp
@@ -1,4 +1,16 @@
 #include "my_vector.h"
 
+using std::cout;
 //
 
+Vector :: Vector(int s)
+:capacity(s), elements{new int [s]}
+{
+    cout<<"memory allocated at: "<<elements<<"\n";
+}
+
+Vector :: ~Vector()
+{
+    delete[] elements;
+    cout<<"Delete memory at: "<<elements<<"\n";
+}
--- src/examples/11_memory/06_vector/my_vector.h
+++ src/examples/11_memory/06_vector/my_vector.h
@@ -1,1 +1,19 @@
 //
+#include <iostream>
+
+#ifndef MY_VECTOR_H
+#define MY_VECTOR_H
+
+class Vector
+{
+public:
+    Vector(int size);
+    ~Vector();
+private:
+    int size{0};//keep track of current index & num of elements in list
+    int capacity; //Max# of elements list can stor
+    int* elements;//list of elements in vector
+};
+
+
+#endif
